2c180dbd8eb8f6f5f071d9b9caf14091988b608a
YeeHHa <matthew.cradit@gmail.com>
2026-01-23 19:29:35 -0500

    updating interactions struct

--- src/discord_data_structs.rs
+++ src/discord_data_structs.rs
@@ -6,11 +6,30 @@ pub struct Interaction {
     pub id: String,
     pub application_id: String,
     pub r#type: u8,
+    pub data: Option<AppCommand>,
+    #[serde(skip)]
+    pub guild: String,
+    pub guild_id: Option<String>,
+    #[serde(skip)]
+    pub channel: Option<String>,
+    pub channel_id: Option<String>,
+    #[serde(skip)]
+    pub member: Option<String>,
+    pub user: User,   
     pub token: u8,
     pub version: u8,
-    pub guild_id: Option<String>,
-    pub channel_id: Option<String>,
-    pub data: Option<AppCommand>   
+    #[serde(skip)]
+    pub message: Option<String>,
+    pub app_permissions: String,
+    pub locale: Option<String>,
+    pub guild_locale: Option<String>,
+    #[serde(skip)]
+    pub entitlements: Vec<String>,
+    #[serde(skip)]
+    pub authorizing_integration_owners: Vec<String>, 
+    #[serde(skip)]
+    pub context: Option<String>,
+    pub attachment_size_limit: u32,
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AppCommand {
@@ -42,5 +61,38 @@ pub struct Command {
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Commands {
     pub commands: Vec<Command>
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct User {
+    pub avatar: String,
+    #[serde(skip)]
+    pub avatar_decoration_data: Option<AvatarDecorationData>,
+	pub bot: bool,
+    #[serde(skip)]
+	pub clan: Option<String>,
+    #[serde(skip)]
+    pub collectibles: Option<String>,
+	pub discriminator: String,
+    #[serde(skip)]
+	pub display_name_styles: Option<String>,
+	pub global_name: String,
+	pub id: String,
+    #[serde(skip)]
+	pub primary_guild: Option<String>,
+	pub public_flags: u32,
+	pub system: bool,
+    #[serde(skip)]
+    pub mfa_enabled: Option<bool>,
+    #[serde(skip)]
+    pub banner: Option<String>,
+    pub locale: String,
+	pub username: String, 
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct AvatarDecorationData {
+    pub asset: String,
+    pub sku_id: String
 }
 
