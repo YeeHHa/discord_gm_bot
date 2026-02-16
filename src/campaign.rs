0c550ecf1940ff7725702b1cfeae2665cfccad08
YeeHHa <matthew.cradit@gmail.com>
2026-02-16 17:44:06 -0500

    Updating campaign

--- src/campaign.rs
+++ src/campaign.rs
@@ -7,6 +7,7 @@ use std::time::Instant;
 pub struct Campaign {
     pub active: bool,
     pub players: Vec<Player>,
+    pub current_player : Option<Player>,
     pub channel_id: String,
     pub creation: Instant 
     
@@ -16,9 +17,29 @@ impl Campaign {
     pub fn new(channel_id: &str) -> Campaign {
         Campaign { 
             active: false, 
-            players: Vec::new(), 
+            players: Vec::new(),
+            current_player: None, 
             channel_id: channel_id.to_string(),
             creation: Instant::now() 
+        }
+    }
+
+    pub fn next_player(&mut self) {
+
+        match self.current_player.as_mut() {
+            Some(current) => {
+                let current_index = self.players.iter().position(|p| p.id == current.id);
+                if let Some(index) = current_index {
+                    let next_index = (index + 1) % self.players.len();
+                    self.current_player = Some(self.players[next_index].clone());
+                }
+            },
+            None => { 
+                if !self.players.is_empty() {
+                    self.current_player = Some(self.players[0].clone());
+                }
+            }
+            
         }
     }
 }
